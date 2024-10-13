fc313dc3256a8ba7defce2fc32451f5cca1837a8
Tiramisu <165537498+darwinx64@users.noreply.github.com>
2024-10-12 19:56:46 -0500

    feat: add setAuthor to embeds

--- Sources/DDBKitUtilities/Message/Protocols and Builders/MessageEmbed.swift
+++ Sources/DDBKitUtilities/Message/Protocols and Builders/MessageEmbed.swift
@@ -124,4 +124,5 @@ public extension MessageEmbed {
   func setColor(_ color: DiscordColor) -> Self { var e = self; e.color = color; return e }
   func setURL(_ url: String) -> Self { var e = self; e.url = url; return e }
   func setProvider(_ name: String, url: String? = nil) -> Self { var e = self; e.provider = .init(name: name, url: url); return e }
+  func setAuthor(_ name: String, url: String? = nil, icon_url: DynamicURL? = nil) -> Self { var e = self; e.author = Embed.Author(name: name, url: url, icon_url: icon_url); return e }
 }
