81e39f7dd32acdf9fc4ae95fcd8858391ec0b7c5
darwinx64 <165537498+darwinx64@users.noreply.github.com>
2025-02-20 18:24:10 -0600

    feat: option abstraction

--- Sources/DDBKitUtilities/Abstraction Extensions/InteractionExtras/Interactions.swift
+++ Sources/DDBKitUtilities/Abstraction Extensions/InteractionExtras/Interactions.swift
@@ -1,10 +1,11 @@
 //
-//  Gateway+CommandResponses.swift
+//  Interactions.swift
 //  DDBKit
 //
 //  Created by Lakhan Lothiyi on 26/09/2024.
 //
 
+import DiscordBM
 import DDBKit
 
 public extension InteractionExtras {
@@ -96,5 +97,60 @@ public extension InteractionExtras {
   func deleteResponse() async throws {
     try await self.client.deleteOriginalInteractionResponse(token: interaction.token)
       .guardSuccess()
+  }
+  
+  // MARK: Options
+  
+  /// Get the value given for a user option.
+  /// Returns `nil` if it was not provided for an optional option, or if DiscordBM could not get the value
+  func getUser(from option: String) async -> DiscordUser? {
+    guard let id = try? self.options?.option(named: option)?.requireString() else { return nil }
+    guard let value = try? await self.client.getUser(id: .init(id)).decode() else { return nil }
+    return value
+  }
+  
+  /// Get the value given for a role option.
+  /// Returns `nil` if it was not provided for an optional option, or if DiscordBM could not get the value
+  func getRole(from option: String) async -> Role? {
+    guard let id = try? self.options?.option(named: option)?.requireString() else { return nil }
+    // guild id will probably never be nil
+    // you can use commands with role options in DMs, they are just broken and will not work no matter what
+    guard let value = try? await self.client.listGuildRoles(id: self.interaction.guild_id!).decode() else { return nil }
+    return value.first { $0.id.rawValue == id }
+  }
+  
+  /// Get the value given for a string option.
+  /// Returns `nil` if it was not provided for an optional option, or if DiscordBM could not get the value
+  func getString(from option: String) async -> String? {
+    guard let value = try? self.options?.option(named: option)?.requireString() else { return nil }
+    return value
+  }
+  
+  /// Get the value given for a bool option.
+  /// Returns `nil` if it was not provided for an optional option, or if DiscordBM could not get the value
+  func getBool(from option: String) async -> Bool? {
+    guard let value = try? self.options?.option(named: option)?.requireBool() else { return nil }
+    return value
+  }
+  
+  /// Get the value given for an int option.
+  /// Returns `nil` if it was not provided for an optional option, or if DiscordBM could not get the value
+  func getInteger(from option: String) async -> Int? {
+    guard let value = try? self.options?.option(named: option)?.requireInt() else { return nil }
+    return value
+  }
+  
+  /// Get the value given for a double option.
+  /// Returns `nil` if it was not provided for an optional option, or if DiscordBM could not get the value
+  func getDouble(from option: String) async -> Double? {
+    guard let value = try? self.options?.option(named: option)?.requireDouble() else { return nil }
+    return value
+  }
+  
+  /// Get the value given for an attachment option.
+  /// Returns `nil` if it was not provided for an optional option, or if DiscordBM could not get the value
+  func getAttachment(from option: String) async -> DiscordChannel.Message.Attachment? {
+    guard let value = await self.getString(from: option) else { return nil }
+    return try? self.attachments.values.first { $0.id.rawValue == value }
   }
 }
