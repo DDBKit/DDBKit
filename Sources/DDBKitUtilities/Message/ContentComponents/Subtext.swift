c5ae6ff1f2013757421fc4bbc099b753a6a096f0
darwinx64 <165537498+darwinx64@users.noreply.github.com>
2025-02-21 11:09:52 -0600

    feat: subtext

---
+++ Sources/DDBKitUtilities/Message/ContentComponents/Subtext.swift
@@ -0,0 +1,19 @@
+//
+//  Subtext.swift
+//  DDBKit
+//
+//  Created by tiramisu on 2/21/25.
+//
+
+import Foundation
+
+public struct Subtext: MessageContentComponent {
+  var txt: String
+  public init(_ str: String) {
+    self.txt = str
+  }
+  
+  public var textualRepresentation: String {
+    return "-# \(txt)\n"
+  }
+}
